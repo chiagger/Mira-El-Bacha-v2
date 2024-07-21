1d07bac3713148b2e60499649617a60450b66f59
chiagger <busca.chiara.cb@gmail.com>
2024-07-21 16:37:21 +0200

    landing page completed

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../style/MainContent.css";
 import "../style/Scrollbar.css";
 import {
@@ -9,8 +9,83 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import PVC1 from "../assets/img/PVC1.jpg";
+import PVC2 from "../assets/img/PVC2.jpg";
+import PVC3 from "../assets/img/PVC3.jpg";
+import PVC4 from "../assets/img/PVC4.jpg";
+import jonah1 from "../assets/img/jonah1.jpg";
+import jonah2 from "../assets/img/jonah2.jpg";
+import third1 from "../assets/img/3.jpg";
+import third2 from "../assets/img/4.jpg";
+import fourth1 from "../assets/img/5.jpg";
+import fourth2 from "../assets/img/6.jpg";
+import fifth1 from "../assets/img/7.jpg";
+import fifth2 from "../assets/img/8.jpg";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const MainContent: React.FC = () => {
+  const PVCImages = [PVC1, PVC2, PVC3, PVC4];
+  const [currentPVC, setCurrentPVC] = React.useState(0);
+
+  const jonahImages = [jonah1, jonah2];
+  const [currentJonah, setCurrentJonah] = React.useState(0);
+
+  const thirdImages = [third1, third2];
+  const [currentThird, setCurrentThird] = React.useState(0);
+
+  const fourthImages = [fourth1, fourth2];
+  const [currentFourth, setCurrentFourth] = React.useState(0);
+
+  const fifthImages = [fifth1, fifth2];
+  const [currentFifth, setCurrentFifth] = React.useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentJonah((prevIndex) => (prevIndex + 1) % jonahImages.length);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, [jonahImages.length]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentPVC((prevIndex) => (prevIndex + 1) % PVCImages.length);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, [PVCImages.length]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentThird((prevIndex) => (prevIndex + 1) % thirdImages.length);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, [thirdImages.length]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentFourth((prevIndex) => (prevIndex + 1) % fourthImages.length);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, [fourthImages.length]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentFifth((prevIndex) => (prevIndex + 1) % fifthImages.length);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, [fifthImages.length]);
+
   return (
     <main
       className="main-content"
@@ -24,39 +99,182 @@ const MainContent: React.FC = () => {
       <ScrollArea
         className="rounded-md border p-4"
         style={{
-          width: "45%",
+          width: "50%",
           height: "85%",
         }}
       >
         <div
           style={{
             display: "grid",
-            gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))",
-            gap: "1rem",
+            gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+            gap: "0.5rem",
           }}
         >
-          <Card>
-            <CardContent>ok</CardContent>
-          </Card>
-          <Card>
-            <CardContent>ok</CardContent>
-          </Card>
-          <Card>
-            <CardContent>ok</CardContent>
-          </Card>
-          <Card>
-            <CardContent>ok</CardContent>
-          </Card>
-          <Card>
-            <CardContent>ok</CardContent>
-          </Card>
+          <Dialog>
+            <DialogTrigger>
+              <Card style={{ cursor: "pointer" }}>
+                <CardContent style={{ padding: 4 }}>
+                  <img
+                    src={PVCImages[currentPVC]}
+                    style={{ borderRadius: "5px" }}
+                  />
+                </CardContent>
+              </Card>
+            </DialogTrigger>
+            <DialogContent style={{ padding: 0, paddingBottom: "1.2rem" }}>
+              <img
+                src={PVCImages[currentPVC]}
+                style={{ borderRadius: "10px" }}
+              />
+              <DialogHeader style={{ textAlign: "center" }}>
+                <DialogTitle>PVC Real Estate</DialogTitle>
+                <DialogDescription>directed by Holly Hunter</DialogDescription>
+                <DialogDescription
+                  style={{ paddingLeft: "1rem", paddingRight: "1rem" }}
+                >
+                  A music video for the song PVC Real Estate by Shelf Lives.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          <Dialog>
+            <DialogTrigger>
+              <Card style={{ cursor: "pointer" }}>
+                <CardContent style={{ padding: 4 }}>
+                  <img
+                    src={jonahImages[currentJonah]}
+                    style={{ borderRadius: "5px" }}
+                  />
+                </CardContent>
+              </Card>
+            </DialogTrigger>
+            <DialogContent style={{ padding: 0, paddingBottom: "1.2rem" }}>
+              <img
+                src={jonahImages[currentJonah]}
+                style={{ borderRadius: "10px" }}
+              />
+              <DialogHeader style={{ textAlign: "center" }}>
+                <DialogTitle>Datura Daydreams</DialogTitle>
+                <DialogDescription>
+                  directed by Jonah Garrett-Bannister
+                </DialogDescription>
+                <DialogDescription
+                  style={{ paddingLeft: "1rem", paddingRight: "1rem" }}
+                >
+                  Sam is bored of hookup culture, and while trying to find the
+                  balance between superficial appearance and genuine attraction
+                  he discovers an escape: an ugly thorned flower where he
+                  convinces himself the man of his dreams resides.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          <Dialog>
+            <DialogTrigger>
+              <Card style={{ cursor: "pointer" }}>
+                <CardContent style={{ padding: 4 }}>
+                  <img
+                    src={thirdImages[currentThird]}
+                    style={{ borderRadius: "5px" }}
+                  />
+                </CardContent>
+              </Card>
+            </DialogTrigger>
+            <DialogContent style={{ padding: 0, paddingBottom: "1.2rem" }}>
+              <img
+                src={thirdImages[currentThird]}
+                style={{ borderRadius: "10px" }}
+              />
+              <DialogHeader style={{ textAlign: "center" }}>
+                <DialogTitle>Rise Like Water</DialogTitle>
+                <DialogDescription>
+                  directed by Jonah Garrett-Bannister
+                </DialogDescription>
+                <DialogDescription
+                  style={{ paddingLeft: "1rem", paddingRight: "1rem" }}
+                >
+                  Lena comes across a group of nomads in London who help her
+                  come to terms with her troubled past by connecting her to
+                  spirit and balance. By turning her back on the establishment,
+                  she ultimately sees her connection to the natural world and
+                  learns the need to fight for it.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          <Dialog>
+            <DialogTrigger>
+              <Card style={{ cursor: "pointer" }}>
+                <CardContent style={{ padding: 4 }}>
+                  <img
+                    src={fourthImages[currentFourth]}
+                    style={{ borderRadius: "5px" }}
+                  />
+                </CardContent>
+              </Card>
+            </DialogTrigger>
+            <DialogContent style={{ padding: 0, paddingBottom: "1.2rem" }}>
+              <img
+                src={fourthImages[currentFourth]}
+                style={{ borderRadius: "10px" }}
+              />
+              <DialogHeader style={{ textAlign: "center" }}>
+                <DialogTitle>Morning After</DialogTitle>
+                <DialogDescription>
+                  directed by Saoirse D. Byrne
+                </DialogDescription>
+                <DialogDescription
+                  style={{ paddingLeft: "1rem", paddingRight: "1rem" }}
+                >
+                  Three queer friends make their way home from a night out.
+                  Throughout the film, they will question their own
+                  self-censorship, and the place in which it is coming from – is
+                  it out of fear, embarrassment, self-preservation, or a
+                  combination of the three? And how can we liberate ourselves
+                  from this judgment not only from others but from ourselves,
+                  and become comfortable in being unapologetically queer?
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          <Dialog>
+            <DialogTrigger>
+              <Card style={{ cursor: "pointer" }}>
+                <CardContent style={{ padding: 4 }}>
+                  <img
+                    src={fifthImages[currentFifth]}
+                    style={{ borderRadius: "5px" }}
+                  />
+                </CardContent>
+              </Card>
+            </DialogTrigger>
+            <DialogContent style={{ padding: 0, paddingBottom: "1.2rem" }}>
+              <img
+                src={fifthImages[currentFifth]}
+                style={{ borderRadius: "10px" }}
+              />
+              <DialogHeader style={{ textAlign: "center" }}>
+                <DialogTitle>Fragile</DialogTitle>
+                <DialogDescription>directed by Laura Esteban</DialogDescription>
+                <DialogDescription
+                  style={{ paddingLeft: "1rem", paddingRight: "1rem" }}
+                >
+                  A music video for the song Fragile by Noor Mandviwalla.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
       </ScrollArea>
       <ScrollArea
         className="rounded-md border p-4"
-        style={{ width: "45%", height: "85%" }}
+        style={{ width: "40%", height: "85%" }}
       >
-        <Card style={{ textAlign: "left" }}>
+        <Card style={{ textAlign: "left", fontSize: "0.9em" }}>
           <CardHeader style={{ textAlign: "center" }}>
             <CardTitle>Welcome!</CardTitle>
             <CardDescription>
